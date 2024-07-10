ac4d0853529b996870e48e1fe057bc96a719f6c4
Jaswanth Singh Kumar Lankadasu <91729041+jaswanthlsvyj@users.noreply.github.com>
2024-07-10 20:21:25 +0530

    Delete bin directory

--- bin/activate.csh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fe6a15878ccfa28092ab79134cd55c56bcce78d82764a48ed53c99eeda3777b1
-size 1012
--- bin/activate.fish
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8fc54ddac1680ec16b44ecbf106bbd3db5a726463a26aa4ed2c717ce9a30a461
-size 2292
